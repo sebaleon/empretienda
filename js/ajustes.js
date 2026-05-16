7bf2a5acfa884a9b78d561f958cf9104bbeb9f23
sebaleon <datosslc84@gmail.com>
2026-05-16 01:10:18 -0300

    Update ajustes.js

--- js/ajustes.js
+++ js/ajustes.js
@@ -1,37 +1,3 @@
-// 30 descuento
-document.addEventListener("DOMContentLoaded", function(event) {
-  
-////
-function getRandomImage(arr) {
-  const length = arr.length;
-  const randomIndex = Math.floor(length * Math.random())
-  return arr[randomIndex]
-}
-let img=[    {src:'https://d22fxaf9t8d39k.cloudfront.net/ac434dae6d18533b754811ffed245ff8e63bb4cf1c8a1c3961081aea4f53c68031350.jpg',},{src:'https://i.imgur.com/r6GlbT9.jpg',},{src:'https://i.imgur.com/pV3Rct8.jpg',},{src:'https://i.imgur.com/oRutIOk.png',},{src:'https://i.imgur.com/bf1KAWz.jpeg',},{src:'https://i.imgur.com/22wjQDw.jpg',},{src:'https://i.imgur.com/QftH2KH.jpeg',},];
-let lis = document.querySelector('.block-grill-images__grid-item--full').getElementsByTagName('img');
-[].forEach.call(lis, (element, index) => {
-  if (element.className == 'block-grill-images__image') {  
-      //ramdom      
-      document.querySelector('.block-grill-images__image').src = 
-      getRandomImage(img)['src'];}});
-
-});
-
-// destaca personalizados y papeleria
-window.addEventListener("load", (event) => {
-  // PC
-  let lis = document.querySelector('.header-menu__desktop-list').getElementsByTagName('a');
-  [].forEach.call(lis, (element, index) => {
-    //console.log(element.innerText.trim());
-    if (element.innerText.trim() == 'PERSONALIZADOS') {
-       document.getElementsByClassName("desktop-list__link")[index].target = "_self";
-       //element.style.color = '#ffffff';
-           element.style.backgroundColor = '#00fff7';
-           element.style.backgroundColor = '#5FA6CF';
-    } 
-  });
-});
-
 ///////////////////////
 // ocultar-sin-stock.js
 (() => {
